e0ee19e04f643eabc67ac6d496424f7bcc43de85
Juan Nunez-Iglesias <jni@janelia.hhmi.org>
2012-08-26 22:30:30 -0400

    Bug fix: start with all false positives
    The confusion matrix was starting out with all calls being negative,
    whereas they are in fact positive. There's a reason it's called a
    confusion matrix!
    
    Additionally, include the cython-compiled .c file to remove the
    dependency on Cython.

--- sklearn/metrics/_metrics.pyx
+++ sklearn/metrics/_metrics.pyx
@@ -5,11 +5,12 @@ import numpy as np
 cnp.import_array()
 
 @cython.boundscheck(False)
-cdef cnp.float64_t count_ones(cnp.ndarray[cnp.float64_t, ndim=1, mode='c'] a):
+cdef count_ones(cnp.ndarray[cnp.float64_t, ndim=1, mode='c'] a):
     cdef int i
-    cdef cnp.float64_t count
+    cdef double count
     for i in range(a.shape[0]):
-        if a[i] == 1: count += 1
+        if a[i] == 1.0:
+            count += 1.0
     return count
 
 @cython.boundscheck(False)
@@ -21,18 +22,18 @@ cpdef cnp.ndarray[cnp.float64_t, ndim=1, mode='c'] continuous_confusion(
     cdef int curr_threshold = 0
     cdef int num_samples = sorted_y_true.shape[0]
     cdef int it = 0, itp = 1, itn = 2, ifp = 3, ifn = 4
-    cdef cnp.float64_t pr = sorted_probas_pred[0], tp = 0, \
-        fp = 0, fn = count_ones(sorted_y_true), tn = num_samples - fn
+    cdef cnp.float64_t pr = sorted_probas_pred[0], tn = 0, \
+        fn = 0, tp = count_ones(sorted_y_true), fp = num_samples - tp
     cdef cnp.ndarray[cnp.float64_t, ndim=2, mode='c'] confusion
     confusion = np.zeros((len(sorted_y_true), 5), dtype=np.float64)
     for i in range(num_samples):
         pr = sorted_probas_pred[i]
         if sorted_y_true[i] == 1:
-            tp += 1
-            fn -= 1
+            tp -= 1
+            fn += 1
         else:
-            fp += 1
-            tn -= 1
+            fp -= 1
+            tn += 1
         confusion[i, 0] = pr
         confusion[i, 1] = tp
         confusion[i, 2] = tn
